4aadb79abe3015799775b106dcc98019e4a41513
othongonzaga <othoncesar1@hotmail.com>
2022-07-16 16:24:20 -0300

    Fazendo o código do CalculaIdade

---
+++ calculaIdade/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Digite seu nome: ");
+String nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}!");
+
+Console.Write("Digite o ano de nascimento: ");
+int ano = int.Parse(Console.ReadLine());
+int idade = 2022 - ano;
+
+Console.WriteLine($"Você tem {idade} anos.");
+
+if(idade >= 18){
+    Console.WriteLine("Você é maior de idade");
+}
+else{
+    Console.WriteLine("Você é menor de idade");
+}
