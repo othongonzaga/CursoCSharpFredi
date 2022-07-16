f353fa9c45467ed6c545da567a4f6ddc7e42933a
othongonzaga <othoncesar1@hotmail.com>
2022-07-16 20:58:48 -0300

    Fazendo o código do loop

---
+++ codigos/loop/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program{
+    static void Main(){
+        string[] nomes = {"Fredi", "Mariana", "Iaabella"};
+
+        for (int i = 0; i < 3; i++){
+            Console.WriteLine(nomes[i]);
+        }
+
+        // Ou
+
+        foreach (string nome in nomes){
+            Console.WriteLine(nome);
+        }
+    }
+}
